747aed5e170e79517d825391a76478330e3d02b8
AndroidPie <liberasoftware@gmail.com>
2019-12-25 16:27:11 +0100

    in process of adding search filter to firebase data

--- src/app/listed-content/firebase.service.ts
+++ src/app/listed-content/firebase.service.ts
@@ -1,6 +1,6 @@
 import {Injectable} from '@angular/core';
 import {AngularFirestore} from '@angular/fire/firestore';
-import {Observable} from 'rxjs';
+import {combineLatest, merge, Observable} from 'rxjs';
 import {NavigationService} from '../navigation/navigation.service';
 import {flatMap, switchMap, take} from 'rxjs/operators';
 import {ItemModel} from './item.model';
@@ -16,17 +16,21 @@ export class FirebaseService {
 
   constructor(
     private firestore: AngularFirestore,
-    private mainService: NavigationService
+    private navigationService: NavigationService
   ) {
-    this.data$ = mainService.listedContentSubject.pipe(
+    this.data$ = navigationService.listedContentSubject.pipe(
       flatMap(collectionKey =>
         this.firestore.collection(collectionKey).valueChanges() as Observable<ItemModel[]>
       )
     );
   }
 
+  getFirebaseDataFiltered() {
+    return combineLatest(this.data$, this.navigationService.searchedKeyword$);
+  }
+
   updateItem(item: PrayerModel | ThoughtModel): Observable<void> {
-    return this.mainService.listedContentSubject.pipe(
+    return this.navigationService.listedContentSubject.pipe(
       take(1),
       switchMap(collectionKey => this.firestore.collection(collectionKey).doc(item.title).set(item))
     );
